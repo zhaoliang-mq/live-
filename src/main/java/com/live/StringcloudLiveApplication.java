eb175844eb0d4f0985d381ac5824d8934c9a5a0f
zl <63003413@qq.com>
2020-08-18 21:27:38 +0800

    提交

---
+++ src/main/java/com/live/StringcloudLiveApplication.java
@@ -0,0 +1,13 @@
+package com.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringcloudLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringcloudLiveApplication.class, args);
+    }
+
+}
---
+++ stringcloud-zuul/src/test/java/com/live/StringcloudZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringcloudZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
