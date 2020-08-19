dd363a6d344f302493d15c0fd742ab13513e7c0b
zhaoliang-mq <69137905+zhaoliang-mq@users.noreply.github.com>
2020-08-19 17:25:26 +0800

    zf
    礼物后台管理

--- springboot-gift/src/main/java/com/fh/gift/service/giftService.java
+++ springboot-gift/src/main/java/com/fh/gift/service/giftService.java
@@ -1,4 +1,10 @@
 package com.fh.gift.service;
 
+import com.fh.gift.common.ResponseCode;
+import com.fh.gift.model.Gift;
+
 public interface giftService {
+    ResponseCode addGift(Gift gift);
+
+    ResponseCode queryGiftList();
 }
--- springboot-gift/src/main/java/com/fh/gift/service/giftServiceImpl.java
+++ springboot-gift/src/main/java/com/fh/gift/service/giftServiceImpl.java
@@ -1,14 +1,30 @@
 package com.fh.gift.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.fh.gift.common.ResponseCode;
 import com.fh.gift.mapper.giftMapper;
+import com.fh.gift.model.Gift;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class giftServiceImpl implements giftService {
 
     @Resource
     private giftMapper giftMapper;
+
+    @Override
+    public ResponseCode addGift(Gift gift) {
+        giftMapper.insert(gift);
+        return ResponseCode.success();
+    }
+
+    @Override
+    public ResponseCode queryGiftList() {
+        List<Gift> giftList = giftMapper.selectList(null);
+            return ResponseCode.success(giftList);
+    }
 }
 
