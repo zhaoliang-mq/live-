23b6096a66db155a034b4a663391c9ec03c925e9
zhaoliang-mq <69137905+zhaoliang-mq@users.noreply.github.com>
2020-08-19 17:23:27 +0800

    zf
    礼物后台管理

--- springboot-gift/src/main/java/com/fh/gift/controller/giftController.java
+++ springboot-gift/src/main/java/com/fh/gift/controller/giftController.java
@@ -1,5 +1,7 @@
 package com.fh.gift.controller;
 
+import com.fh.gift.common.ResponseCode;
+import com.fh.gift.model.Gift;
 import com.fh.gift.service.giftService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,4 +14,15 @@ public class giftController {
     @Autowired
     private giftService giftService;
 
+    //添加礼物
+    @RequestMapping("addGift")
+    public ResponseCode addGift(Gift gift){
+        return giftService.addGift(gift);
+    }
+
+    //查询礼物数据礼物
+    @RequestMapping("queryGiftList")
+    public ResponseCode queryGiftList(){
+        return giftService.queryGiftList();
+    }
 }
